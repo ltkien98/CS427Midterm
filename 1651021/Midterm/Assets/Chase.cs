62a4c0a36edb808be9ac4bc7287212e43d333922
ltkien98 <ltkien@apcs.vn>
2019-07-21 10:15:25 +0700

    final push

---
+++ 1651021/Midterm/Assets/Chase.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Chase : MonoBehaviour
+{
+    public string level;
+    public float speed;
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        GetComponent<Animator>().SetTrigger("Chase");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(level);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # CS427Midterm
 midterm
+
+Student ID: 1651021
+Full name: Le Trung Kien
+
+My idea for the game is a platform quiz solve with hidden trap and chasing monster AI.
+Most of the game is inspired by Maplestory mobs and character
+
+Basically the player has to understand the hint and follow it because the trigger to next level is hidden somewhere in the scene.
+
+In this project I use basic physical controlling with 4 directional key and space bar
+
+Right click is used to show the hint for each level
+
+The game is Created for Window platform with Unity 2019.1.4f1 (64-bit)
